60aa482a2ed9a9db56cbb2140646ace77a2d42f7
Tharaniben <tharanben192003@gamil.com>
2026-04-21 14:15:51 +0530

    (setup):initial repo structure

---
+++ src/main/java/com/fooddelivery/onlinefooddeliverymanagementsystem/OnlineFoodDeliveryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.onlinefooddeliverymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodDeliveryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFoodDeliveryManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fooddelivery/onlinefooddeliverymanagementsystem/OnlineFoodDeliveryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddelivery.onlinefooddeliverymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodDeliveryManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
